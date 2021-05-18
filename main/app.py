08104ba0c236f650a1ef072cc6c010195c402a02
KaiXavier <akhil.b.xavier3247@gmail.com>
2021-05-18 13:32:51 +0530

    added register, login page

--- main/app.py
+++ main/app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return'hello,world!'
+def home():
+    return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
