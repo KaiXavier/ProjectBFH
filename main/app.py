d8fec43c2f1e9191ff7a393d9aefa959daaa0b72
KaiXavier <akhil.b.xavier3247@gmail.com>
2021-05-19 11:01:38 +0530

    added database

--- main/app.py
+++ main/app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy, model
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///eventportal.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class addevent(db.Model):
+    sno = db.Column(db.Integer, primary_key= True)
+    eventName = db.Column(db.String(200), nullable= False)
+    date = db.Column(db.Integer, nullable= False)
+    desc = db.Column(db.String(500), nullable= False)
+    date = db.Column(db.Integer, nullable= False)
+    time = db.Column(db.Integer, nullable= False)
+    place = db.Column(db.String(200), nullable= False)
+    max_member = db.Column(db.Integer, nullable= False)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.eventName} - {self.date} - {self.time}"
 
 @app.route('/')
 def home():
@@ -8,6 +26,7 @@ def home():
 
 @app.route('/register')
 def register():
+    # newEvent = addevent()
     return render_template('register.html')
 
 @app.route('/login')
