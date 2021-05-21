d9404b10b500f7db75f7e2f4abb32bf34b326baf
KaiXavier <akhil.b.xavier3247@gmail.com>
2021-05-21 10:24:29 +0530

    new update

--- main/app.py
+++ main/app.py
@@ -3,14 +3,13 @@ from flask_sqlalchemy import SQLAlchemy, model
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///eventportal.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///eventDate.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class addevent(db.Model):
     sno = db.Column(db.Integer, primary_key= True)
     eventName = db.Column(db.String(200), nullable= False)
-    date = db.Column(db.Integer, nullable= False)
     desc = db.Column(db.String(500), nullable= False)
     date = db.Column(db.Integer, nullable= False)
     time = db.Column(db.Integer, nullable= False)
@@ -24,10 +23,22 @@ class addevent(db.Model):
 def home():
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods = ['GET', 'POST'])
 def register():
-    # newEvent = addevent()
-    return render_template('register.html')
+    if request.method == 'POST':
+        eventName = request.form['eventName']
+        date = request.form['date']
+        time = request.form['time']
+        max_member = request.form['max_member']
+        desc = request.form['desc']
+        place = request.form['place']
+
+        newEvent = addevent(eventName =eventName, date = date, time = time, max_member = max_member, desc = desc, place = place)
+        db.session.add(newEvent)
+        db.session.commit()
+
+    allEvents = addevent.query.all()
+    return render_template('register.html', allEvents = allEvents)
 
 @app.route('/login')
 def login():
