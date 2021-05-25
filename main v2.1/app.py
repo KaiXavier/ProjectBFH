6faabb07ac01eed898ef42805437e0eee9fe4e95
KaiXavier <akhil.b.xavier3247@gmail.com>
2021-05-25 15:29:56 +0530

    next commit

--- main v2.1/app.py
+++ main v2.1/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date, time
 
+
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///eventDate.db"
@@ -10,8 +12,6 @@ db = SQLAlchemy(app)
 
 class addevent(db.Model):
 
-    # uname = db.column(db.String(100), nullable = False)
-    # pswd = db.column(db.String(100), nullable = False)
     sno = db.Column(db.Integer, primary_key= True)
     eventName = db.Column(db.String(200), nullable= False)
     desc = db.Column(db.String(500), nullable= False)
@@ -28,12 +28,20 @@ class addevent(db.Model):
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['uname'] != 'admin@gmail.com' or request.form['pswd'] != 'admin':
+        if request.form['uname'] != db.uname or request.form['pswd'] != db.pswd: #bugged
             error = 'Invalid Credentials. Please try again.'
         else:
             return redirect('/home')
+
+        
     return render_template('login.html', error=error)
     # return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
 
 
 @app.route("/home")
@@ -67,9 +75,29 @@ def delete(sno):
     db.session.commit()
     return redirect('/events')
 
-@app.route("/events")
-def update():
-    return render_template("Events.html")
+@app.route("/update/<int:sno>", methods = ['GET', 'POST'])
+def update(sno):
+    if request.method=='POST':
+        eventName = request.form['eventName']
+        date = request.form['date']
+        time = request.form['time']
+        max_member = request.form['max_member']
+        desc = request.form['desc']
+        place = request.form['place']
+
+        updateEvents = addevent.query.filter_by(sno=sno).first()
+        updateEvents.eventName = eventName
+        updateEvents.date = date
+        updateEvents.time = time
+        updateEvents.max_member = max_member
+        updateEvents.desc = desc
+        updateEvents.place = place
+        db.session.add(updateEvents)
+        db.session.commit()
+        return redirect("/events")
+
+    updateEvents = addevent.query.filter_by(sno=sno).first()
+    return render_template('update.html', updateEvents = updateEvents)
 
 if __name__ == "__main__":
     app.run(debug= True)
